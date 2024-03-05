ffab008aa22145308580d62ac9310d2ca15384a5
RaMandeep2002 <ramandeepsingh1511@gmail.com>
2024-03-05 10:36:21 +0530

    changes in header or porjects coloumn

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,55 +1,91 @@
-import React from 'react'
-import { SocialIcon } from 'react-social-icons';
-import { motion } from 'framer-motion';
-// import Link from 'next/link';
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+// import Link from "next/link";
 
-
-type Props = {}
+type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
-        <motion.div 
-            initial={{
-                x:-500,
-                opacity:0,
-                scale:0.5,
-            }}
-            animate={{
-                x:0,
-                opacity:1,
-                scale:1
-            }}
-            transition={{
-                duration:1.5,
-            }}
-            className='flex flex-row items-center gap-2 '>
-            {/* social Icon */}
-            <SocialIcon url="https://facebook.com/raman-sardar"fgColor='white' bgColor='transparent' className="hover:bg-blue-500 rounded-full"/>
-            <SocialIcon url="https://instagram.com/_sardarji_15" bgColor='transparent' className="hover:bg-gradient-to-r from-fuchsia-600 to-pink-600    rounded-full"/>
-            <SocialIcon url="https://twitter.com/raman2002"  bgColor='transparent' className="hover:bg-blue-300 rounded-full"/>
-            <SocialIcon url="https://github.com/RaMandeep2002" bgColor='transparent' className="hover:bg-blue-300 rounded-full"/>
-            <SocialIcon url="https://linkedin.com/jaketrent"  bgColor='transparent' className="hover:bg-blue-300 rounded-full"/>
-        </motion.div>
-        <motion.div 
-            initial={{
-                x:500,
-                opacity:0,
-                scale:0.5,
-            }}
-            animate={{
-                x:0,
-                opacity:1,
-                scale:1
-            }}  
-            transition={{
-                duration:1.5,
-            }} className='flex flex-row items-center text-gray-300 cursor-pointer'>
-            <SocialIcon className="cursor-pointer" network='email' fgColor='gray' bgColor='transparent'/>
-        <p className='uppercase hidden md:inline-flex text-sm text-gray-400 '>Get In Touch</p>
-        <a href="https://drive.google.com/file/d/1LB76BmmkGn4pg2spJ0xGQJRdWXEasQh9/view?usp=drive_link" className='downloadbutton ml-3 uppercase hidden md:inline-flex text-sm text-red-400 '>Download CV</a>
-       
-        </motion.div>
+    <header className="sticky top-0 z-20 mx-auto flex max-w-7xl items-start justify-between p-5 xl:items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center gap-2 "
+      >
+        {/* social Icon */}
+        <SocialIcon
+          url="https://facebook.com/raman-sardar"
+          fgColor="white"
+          bgColor="transparent"
+          className="rounded-full hover:bg-blue-500"
+        />
+        <SocialIcon
+          url="https://instagram.com/_sardarji_15"
+          bgColor="transparent"
+          className="rounded-full from-fuchsia-600 to-pink-600    hover:bg-gradient-to-r"
+        />
+        <SocialIcon
+          url="https://twitter.com/raman2002"
+          bgColor="transparent"
+          className="rounded-full hover:bg-blue-300"
+        />
+        <SocialIcon
+          url="https://github.com/RaMandeep2002"
+          bgColor="transparent"
+          className="rounded-full hover:bg-blue-300"
+        />
+        <SocialIcon
+          url="https://linkedin.com/jaketrent"
+          bgColor="transparent"
+          className="rounded-full hover:bg-blue-300"
+        />
+      </motion.div>
+
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex cursor-pointer flex-row items-center text-gray-300"
+      >
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="hidden text-sm uppercase text-gray-400 md:inline-flex ">
+          Get In Touch
+        </p>
+
+        <a
+          href="https://drive.google.com/file/d/1LB76BmmkGn4pg2spJ0xGQJRdWXEasQh9/view?usp=drive_link"
+          className="downloadbutton ml-3 hidden text-sm uppercase text-red-400 md:inline-flex "
+        >
+          Download CV
+        </a>
+      </motion.div>
     </header>
-  )
+  );
 }
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,45 +1,60 @@
-import { motion } from 'framer-motion';
-import React from 'react'
+import { motion } from "framer-motion";
+import React from "react";
 
+type Props = {};
 
-type Props = {}
+function Projects({}: Props) {
+  const projects = [1, 2];
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="relative z-0 mx-auto flex h-screen max-w-full flex-col items-center justify-evenly overflow-hidden text-left md:flex-row"
+    >
+      <h3 className="absolute top-24 text-2xl uppercase tracking-[20px] text-gray-500 ">
+        Projects
+      </h3>
 
-function Projects({ }: Props) {
-    const projects = [1, 2];
-    return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ duration: 1.5 }}
-            className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
-            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl '>Projects</h3>
+      <div className="sanp-x scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 relative z-20 flex w-full snap-mandatory overflow-y-hidden overflow-x-scroll">
+        {projects.map((project, i) => (
+          <>
+            <div className="flex h-screen w-screen flex-shrink-0 snap-center flex-col items-center justify-center space-y-5 p-20 md:p-44 ">
+              <motion.img
+                initial={{ y: -300 }}
+                transition={{ duration: 1.2 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                className="h-24 w-24"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+                alt=""
+              />
 
-            <div className='relative w-full flex overflow-x-scroll overflow-y-hidden sanp-x snap-mandatory z-20 scrollbar scrollbar-thick scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
-                {projects.map((project, i) => (
-                    <>
-                        <div
-                            className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen '>
-                            <motion.img
-                                initial={{ y: -300 }}
-                                transition={{ duration: 1.2 }}
-                                whileInView={{ opacity: 1, y: 0 }}
-                                viewport={{ once: true }}
-                                className='w-24 h-24' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" alt="" />
+              <div className="space-y-18 max-w-6xl px-0 md:px-10 ">
+                <h4 className="text-center text-4xl font-semibold">
+                  {" "}
+                  <span className="underline decoration-[#F7AB0A]/50">
+                    Case Study {i + 1} of {projects.length}:
+                  </span>{" "}
+                  Project name
+                </h4>
 
-                            <div className='space-y-18 px-0 md:px-10 max-w-6xl '>
-                                <h4 className='text-4xl font-semibold text-center'> <span className='underline decoration-[#F7AB0A]/50'>Case Study {i + 1} of {projects.length}:</span>  Project name</h4>
-
-                                <p className='text-lg text-center md:text-left'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, quaerat ea! Repudiandae vel dicta officiis eum qui, necessitatibus ipsa laudantium quaerat, doloribus sunt repellendus veritatis aut. Ratione, fugit! Repellendus, voluptates.</p>
-                            </div>
-                        </div>
-                    </>
-                ))}
+                <p className="text-center text-lg md:text-left">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. In,
+                  quaerat ea! Repudiandae vel dicta officiis eum qui,
+                  necessitatibus ipsa laudantium quaerat, doloribus sunt
+                  repellendus veritatis aut. Ratione, fugit! Repellendus,
+                  voluptates.
+                </p>
+              </div>
             </div>
+          </>
+        ))}
+      </div>
 
-            <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12' />
-        </motion.div>
-
-    )
+      <div className="absolute left-0 top-[30%] h-[500px] w-full -skew-y-12 bg-[#F7AB0A]/10" />
+    </motion.div>
+  );
 }
 
-export default Projects
+export default Projects;
